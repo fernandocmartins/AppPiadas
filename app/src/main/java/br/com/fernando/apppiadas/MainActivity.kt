bfae2a8699ee4fd2ac02a8c2123fb0d5c0db2d1b
Fernando <fernandocsrmartins@gmail.com>
2022-12-13 22:11:08 -0300

    dimens, strings, array-string e tela funcional

--- app/src/main/java/br/com/fernando/apppiadas/MainActivity.kt
+++ app/src/main/java/br/com/fernando/apppiadas/MainActivity.kt
@@ -2,10 +2,30 @@ package br.com.fernando.apppiadas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import br.com.fernando.apppiadas.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btTellJoker.setOnClickListener {
+            showjoker()
+        }
     }
+
+    private fun showjoker() {
+        val jokers = resources.getStringArray( R.array.jokers)
+        val numberJoker = Random().nextInt(jokers.size)
+        val joker = jokers[numberJoker]
+        binding.tvJoker.text = joker
+    }
+
+
 }
